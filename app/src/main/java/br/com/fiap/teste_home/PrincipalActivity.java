3433daccb133ad1fc14bc9f10f823f7717661d84
diferreira <diegoblks@gmail.com>
2017-10-09 12:13:51 -0300

    Teste home estática com botões funcionando, falta apenas alterar o cainho para activitys

---
+++ app/src/main/java/br/com/fiap/teste_home/PrincipalActivity.java
@@ -0,0 +1,36 @@
+package br.com.fiap.teste_home;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+                Intent intentForm = new Intent(PrincipalActivity.this, PrincipalActivity.class);
+                startActivity(intentForm);
+
+                Intent intentFechar = new Intent(PrincipalActivity.this, PrincipalActivity.class);
+                startActivity(intentFechar);
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
